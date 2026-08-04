857d2f86390928a3a7899756050ac724cbecd78f
FarhanAryadi14 <faryadidewani@gmail.com>
2026-08-04 21:55:17 +0700

    ci: set LARAVEL_BYPASS_ENV_CHECK=1 for Vitest CI compatibility

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
+process.env.LARAVEL_BYPASS_ENV_CHECK = '1';
+
 export default defineConfig({
     plugins: [
         laravel({
