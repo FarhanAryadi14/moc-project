325fd9f77738a3421dde8531b7167e25b43513e4
FarhanAryadi14 <faryadidewani@gmail.com>
2026-08-04 21:58:42 +0700

    ci: switch Vitest environment to happy-dom to fix Node 20 undici webidl worker thread crash in GitHub Actions

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,8 @@ export default defineConfig({
     ],
     test: {
         globals: true,
-        environment: 'jsdom',
+        environment: 'happy-dom',
         setupFiles: './resources/js/test/setup.js',
+        pool: 'forks',
     },
 });
